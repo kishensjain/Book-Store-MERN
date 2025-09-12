5f2c94a698f2946dd173f308d4ed40988d321cd9
Kishen S Jain <jain.skishen@gmail.com>
2025-09-12 18:50:32 +0530

    Add cart routes

---
+++ backend/src/controllers/cart.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+export const addToCart = async (req: Request, res: Response) => {}
+
+export const getCart = async (req: Request, res: Response) => {}
+
+export const updateCartItem = async (req: Request, res: Response) => {}
+
+export const removeCartItem = async (req: Request, res: Response) => {}
+
+export const clearCart = async (req: Request, res: Response) => {}
---
+++ backend/src/routes/cart.routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
+
+import { 
+  addToCart, 
+  getCart, 
+  updateCartItem, 
+  removeCartItem, 
+  clearCart 
+} from '../controllers/cart.controller.js';
+
+const router = express.Router();
+
+router.post("/addToCart", authMiddleware, addToCart);
+router.get("/getCart", authMiddleware, getCart);
+router.put("/updateCartItem", authMiddleware, updateCartItem);
+router.delete("/removeCartItem", authMiddleware, removeCartItem);
+router.delete("/clearCart", authMiddleware, clearCart);
+
+export default router;
