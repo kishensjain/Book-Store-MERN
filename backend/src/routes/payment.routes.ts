00e9e9054f02c2de30da3536b38327e3db5231aa
Kishen S Jain <jain.skishen@gmail.com>
2025-09-21 21:11:51 +0530

    Comment webhook for now and start server with npm start

--- backend/src/routes/payment.routes.ts
+++ backend/src/routes/payment.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { createPaymentOrder, verifyPayment, razorpayWebhook } from "../controllers/payment.controller.js"
+import { createPaymentOrder, verifyPayment } from "../controllers/payment.controller.js"
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ router.post("/create", authMiddleware, createPaymentOrder);
 router.post("/verify", authMiddleware, verifyPayment);
 
 // Webhook route (⚠️ no auth middleware here, Razorpay needs direct access)
-router.post("/webhook", express.json({ type: "application/json" }), razorpayWebhook);
+// router.post("/webhook", express.json({ type: "application/json" }), razorpayWebhook);
 
 export default router;
