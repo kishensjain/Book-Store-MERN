2832ce140394bc6d9286dd819664347c6f8e5b64
Kishen S Jain <jain.skishen@gmail.com>
2025-09-27 17:30:53 +0530

    Remove verify-email route

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken, getUserProfile,verifyEmail } from "../controllers/auth.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, getUserProfile } from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -9,7 +9,6 @@ router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/refresh-token", refreshAccessToken);
 router.get("/profile",authMiddleware,getUserProfile);
-router.get("/verify-email", verifyEmail);
 
 
 export default router;
