4b1c17127da4344c7d420d47cb03f96d47872f32
Kishen S Jain <jain.skishen@gmail.com>
2025-10-13 14:08:58 +0530

    feat: implement fetchBooks async thunk and update book slice with extra reducers

--- frontend/src/features/books/bookSlice.ts
+++ frontend/src/features/books/bookSlice.ts
@@ -1,4 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
+import api from "../../api/axios";
 
 interface Book {
   _id: string;
@@ -20,11 +25,37 @@ const initialState: BooksState = {
   error: null,
 };
 
+export const fetchBooks = createAsyncThunk(
+  "books/fetchBooks",
+  async (_, thunkApi) => {
+    try {
+      const response = await api.get("/books");
+      return response.data;
+    } catch (error: any) {
+      return thunkApi.rejectWithValue(
+        error.response?.data?.message || "Failed to fetch books"
+      );
+    }
+  }
+);
 
 const slice = createSlice({
-    name: "book",
-    initialState,
-    reducers:{}
-})
+  name: "book",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchBooks.fulfilled, (state, action: PayloadAction<Book[]>) => {
+        (state.loading = false), (state.books = action.payload);
+      })
+      .addCase(fetchBooks.rejected, (state, action: PayloadAction<any>) => {
+        (state.loading = false), (state.error = action.payload);
+      });
+  },
+});
 
-export default slice.reducer
+export default slice.reducer;
