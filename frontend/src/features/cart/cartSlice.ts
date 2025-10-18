766ae0c2791df7610fe4b4c397cdbc300243f7d1
Kishen S Jain <jain.skishen@gmail.com>
2025-10-18 16:29:40 +0530

    Add cartSlice reducers

--- frontend/src/features/cart/cartSlice.ts
+++ frontend/src/features/cart/cartSlice.ts
@@ -1,25 +1,61 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { act, Activity } from "react";
 
-
-export interface CartItem{
-    bookId : string | null,
-    title : string | null,
-    price : number,
-    coverImage : string | null,
-    quantity : number,
+export interface CartItem {
+  bookId: string | null;
+  title: string | null;
+  price: number;
+  coverImage: string | null;
+  quantity: number;
 }
 
 interface CartState {
   items: CartItem[];
 }
 
-const initialState : CartState = {
-    items : []
-}
-
+const initialState: CartState = {
+  items: [],
+};
 
 const slice = createSlice({
-    name:"cart",
-    initialState,
-    reducers:{}
-})
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action: PayloadAction<CartItem>) {
+      const existingItem = state.items.find(
+        (item) => item.bookId === action.payload.bookId
+      );
+      if (existingItem) {
+        existingItem.quantity += action.payload.quantity;
+      } else {
+        state.items.push(action.payload);
+      }
+    },
+
+    removeFromCart(state, action: PayloadAction<CartItem>) {
+      const existingItem = state.items.find(
+        (item) => item.bookId === action.payload.bookId
+      );
+      if (existingItem) {
+        state.items = state.items.filter(
+          (cartItem) => cartItem.bookId !== action.payload.bookId
+        );
+      }
+    },
+
+    updateCart(state, action: PayloadAction<CartItem>) {
+      let existingItem = state.items.find(
+        (item) => item.bookId === action.payload.bookId
+      );
+      if (existingItem) {
+        Object.assign(existingItem, action.payload)
+      }
+    },
+
+    clearCart(state) {
+      state.items = [];
+    },
+  },
+});
+
+export const {addToCart, removeFromCart, updateCart, clearCart} = slice.actions
